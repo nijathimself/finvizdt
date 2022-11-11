89e7ff312cee94e759357886f614e07f48286eb0
nijathimself <68912981+nijathimself@users.noreply.github.com>
2022-11-11 22:40:46 +0400

    m

--- StockStatus.py
+++ StockStatus.py
@@ -204,13 +204,16 @@ class StockStatusBot(object):
 				final_lowerband[curr] = np.nan
 		
 		supertrend_signal=""
-		if np.isnan(final_lowerband[-1]) and np.isnan(final_lowerband[-2]) and np.isnan(final_lowerband[-3]) and not np.isnan(final_upperband[-1]) and not np.isnan(final_upperband[-2]) and not np.isnan(final_upperband[-3]):
-			supertrend_signal="Sell"
-		elif not np.isnan(final_lowerband[-1]) and not np.isnan(final_lowerband[-2]) and not np.isnan(final_lowerband[-3]) and np.isnan(final_upperband[-1]) and np.isnan(final_upperband[-2]) and np.isnan(final_upperband[-3]):
-			supertrend_signal="Buy"
+		try:
+			if np.isnan(final_lowerband[-1]) and np.isnan(final_lowerband[-2]) and np.isnan(final_lowerband[-3]) and not np.isnan(final_upperband[-1]) and not np.isnan(final_upperband[-2]) and not np.isnan(final_upperband[-3]):
+				supertrend_signal="Sell"
+			elif not np.isnan(final_lowerband[-1]) and not np.isnan(final_lowerband[-2]) and not np.isnan(final_lowerband[-3]) and np.isnan(final_upperband[-1]) and np.isnan(final_upperband[-2]) and np.isnan(final_upperband[-3]):
+				supertrend_signal="Buy"
+			else:
+				supertrend_signal=""
 		else:
-			supertrend_signal=""
-
+			print(some_symbol)
+			
 		return supertrend_signal
 
 	def searchStatus(self, stockSymbolList):
