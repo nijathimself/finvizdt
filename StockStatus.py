9bf36a077c00798a14de03f036efb7c533a7f624
nijathimself <68912981+nijathimself@users.noreply.github.com>
2022-11-13 01:05:58 +0400

    ==

--- StockStatus.py
+++ StockStatus.py
@@ -250,8 +250,8 @@ class StockStatusBot(object):
 			print(signal_1d, end = ' ')
 			print(signal_1w, end = ' ')
 			print(signal_10weeks)
-			
-			if signal_4hr=="Sell" or signal_1d=="Sell" or signal_1w="Sell" or signal_10weeks=="Sell":
+
+			if signal_4hr=="Sell" or signal_1d=="Sell" or signal_1w=="Sell" or signal_10weeks=="Sell":
 				infolist.append(stockSymbol)
 			else:
 				continue
