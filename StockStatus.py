7eef5e9058d57a7c6a6fb42a834313484463803f
nijathimself <68912981+nijathimself@users.noreply.github.com>
2022-11-16 04:23:40 +0400

    satx

--- StockStatus.py
+++ StockStatus.py
@@ -289,7 +289,7 @@ class StockStatusBot(object):
 			print(signal_1w, end = ' ')
 			print(signal_10weeks)
 
-			if signal_4hr=="Sell" or signal_1d=="Sell" or signal_1w=="Sell" or signal_10weeks=="Sell":
+			if signal_4hr=="Sell" or signal_1d=="Sell" or signal_1w=="Sell" or signal_10weeks=="Sell" or stockSymbol=='SATX':
 				infolist.append(stockSymbol)
 			else:
 				continue
