e127dd8c77f8bc6da19c016c39ddee567d67e501
nijathimself <68912981+nijathimself@users.noreply.github.com>
2022-12-04 03:10:32 +0400

    mike email out for test

--- StockStatus.py
+++ StockStatus.py
@@ -311,7 +311,8 @@ class StockStatusBot(object):
 			msg.set_content(mail_content)
 			msg['Subject'] = 'High Risk Stocks'
 			msg['From'] = 'high.risk.stocks@gmail.com'
-			recipients = ['high.risk.stocks@gmail.com', 'mike@mihfinancial.ca']
+			recipients = ['high.risk.stocks@gmail.com']
+			#, 'mike@mihfinancial.ca']
 			msg['To'] = ", ".join(recipients)
 			# Send the message via our own SMTP server.
 			server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
@@ -333,7 +334,8 @@ class StockStatusBot(object):
 			msg.set_content(mail_content)
 			msg['Subject'] = 'Cross referenced with doc file'
 			msg['From'] = 'high.risk.stocks@gmail.com'
-			recipients = ['high.risk.stocks@gmail.com', 'mike@mihfinancial.ca']
+			recipients = ['high.risk.stocks@gmail.com']
+			#, 'mike@mihfinancial.ca']
 			msg['To'] = ", ".join(recipients)
 			# Send the message via our own SMTP server.
 			server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
