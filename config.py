d7e8fc5d4d166c2ff192e68376c6fb244651ec1d
nijathimself <68912981+nijathimself@users.noreply.github.com>
2023-08-17 20:58:53 +0400

    dr

--- config.py
+++ config.py
@@ -41,10 +41,13 @@ class Config(object):
 		options.add_argument("--incognito")
 		options.add_argument("--headless")
 		#options.headless = False
+		
+
 		options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 		print("version:::::::::", os.environ.get("CHROMEDRIVER_VERSION "))
-		service = Service(executable_path=str(os.environ.get('CHROMEDRIVER_PATH')))
-		self.driver = webdriver.Chrome(service=service, options=options)
+		#service = Service(executable_path=str(os.environ.get('CHROMEDRIVER_PATH')))
+		self.driver = webdriver.Chrome(ChromeDriverManager(version="114.0.5735.90").install(), options=options)
+		#self.driver = webdriver.Chrome(service=service, options=options)
 		#self.driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=options,desired_capabilities=capa)
 		#self.driver = webdriver.Chrome(executable_path=path_chrome,options=options,desired_capabilities=capa)
 		#self.driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install(),options=options,desired_capabilities=capa)
