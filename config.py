493020c87de1a3c3a9e9c0f2e9ac511e6430713a
nijathimself <68912981+nijathimself@users.noreply.github.com>
2023-11-09 12:29:18 -0800

    options

--- config.py
+++ config.py
@@ -48,6 +48,13 @@ class Config(object):
 
 		service = Service()
 		options = webdriver.ChromeOptions()
+		options.add_argument('--no-sandbox')
+		options.add_argument('--disable-dev-shm-usage')
+		options.add_argument('start-maximized')
+		options.add_experimental_option("useAutomationExtension", False)
+		options.add_experimental_option("excludeSwitches",["enable-automation"])
+		options.add_argument("--incognito")
+		options.add_argument("--headless")
 		self.driver = webdriver.Chrome(service=service, options=options)
 
 
