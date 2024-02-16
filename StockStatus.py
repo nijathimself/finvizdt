fe265f40c97119b25317058ff41fe2825d7ce889
nijathimself <68912981+nijathimself@users.noreply.github.com>
2024-02-16 13:12:37 -0800

    /

--- StockStatus.py
+++ StockStatus.py
@@ -128,7 +128,7 @@ class StockStatusBot(object):
 		
 		ORG_EMAIL = "@gmail.com" 
 		FROM_EMAIL = "high.risk.stocks" + ORG_EMAIL 
-		FROM_PWD = "narermcwjyamtqpd" 
+		FROM_PWD = "qhyhtfschqvsbwla" 
 		SMTP_SERVER = "imap.gmail.com"
 
 		# Use the client_secret.json file to identify the application requesting
