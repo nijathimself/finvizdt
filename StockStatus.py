da494153d8b6d8b9db2a25db0968c81bf74ed03a
nijathimself <68912981+nijathimself@users.noreply.github.com>
2024-02-24 01:30:46 +0400

    prt

--- StockStatus.py
+++ StockStatus.py
@@ -402,8 +402,13 @@ class StockStatusBot(object):
 					open = df['open']
 				except:
 					if intrval=="5hr_":
-						df=tv.get_hist(some_symbol, exchange=exch, interval = Interval.in_5_minute, n_bars=1500, extended_session=False)
-						time.sleep(5)
+						try:
+							df=tv.get_hist(some_symbol, exchange=exch, interval = Interval.in_5_minute, n_bars=1500, extended_session=False)
+							time.sleep(5)
+							print(df.iloc[0,0])
+						except:
+							df=tv.get_hist(some_symbol, exchange=prefix, interval = Interval.in_5_minute, n_bars=1500, extended_session=False)
+						
 						df['rounded_time'] = df.index.map(self.round_time)
 						df = df.groupby('rounded_time').agg({
 							'open': 'first',
@@ -416,8 +421,13 @@ class StockStatusBot(object):
 						except:
 							print('NO DF')
 					elif intrval=="5hr_ExS":
-						df=tv.get_hist(some_symbol, exchange=exch, interval = Interval.in_5_minute, n_bars=1500, extended_session=True)
-						time.sleep(5)
+						try:
+							df=tv.get_hist(some_symbol, exchange=exch, interval = Interval.in_5_minute, n_bars=1500, extended_session=True)
+							time.sleep(5)
+							print(df.iloc[0,0])
+						except:
+							df=tv.get_hist(some_symbol, exchange=prefix, interval = Interval.in_5_minute, n_bars=1500, extended_session=True)
+						
 						df['rounded_time'] = df.index.map(self.round_time)
 						df = df.groupby('rounded_time').agg({
 							'open': 'first',
@@ -447,7 +457,8 @@ class StockStatusBot(object):
 				print('NO DF')
 			
 			try:
-				five_hour_ALERT=self.HH(final_upperband, open, close, high) 
+				five_hour_ALERT=self.HH(final_upperband, open, close, high)
+				print('5H ALERT for this one is', five_hour_ALERT) 
 			except:
 				five_hour_ALERT=False
 			
@@ -593,6 +604,7 @@ class StockStatusBot(object):
 							five_hour_alert=False
 			else:
 				five_hour_alert=False
+				print('5H Alert is calculated and is', five_hour_alert)
 		except:
 			five_hour_alert=False
 
